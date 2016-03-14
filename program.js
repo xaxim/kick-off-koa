1d4bdbaca71dcba395ca0490ab55af71eb7560f3
Carlos Eduardo Lima Borges <carloslimaborges@gmail.com>
2016-03-14 03:52:11 -0300

    Step 3

--- program.js
+++ program.js
@@ -1,40 +1,29 @@
 var koa = require('koa');
 var app = koa();
+var parse = require('co-body');
 
 //Solução que pensei
-app.use(function *(next) {
-	if (this.path === '/') {
-		this.body = 'hello koa';
-	} else if (this.path === '/404') {
-		this.body = 'page not found';
-	} else if (this.path === '/500') {
-		this.body = 'internal server error';
+app.use(function* (next) {
+	if(this.path !== '/' || this.method !== 'POST') {
+		return yield next;
 	}
+
+	var body = yield parse(this);
+	this.body = body.name.toUpperCase();
 });
 
 //Solução recomendada:
 app.use(function* (next) {
-  if (this.path !== '/') {
-    return yield next;
-  }
+  // only accept POST request
+  if (this.method !== 'POST') return yield next;
 
-  this.body = 'hello koa';
-});
+  // max body size limit to `1kb`
+  var body = yield parse(this, { limit: '1kb' });
 
-app.use(function* (next) {
-  if (this.path !== '/404') {
-    return yield next;
-  }
+  // if body.name not exist, respond `400`
+  if (!body.name) this.throw(400, '.name required');
 
-  this.body = 'page not found';
-});
-
-app.use(function* (next) {
-  if (this.path !== '/500') {
-    return yield next;
-  }
-
-  this.body = 'internal server error';
+  this.body = body.name.toUpperCase();
 });
 
 var port = process.argv[2];
