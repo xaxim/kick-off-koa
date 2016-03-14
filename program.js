a14217dd692070373637acae02de8f3b51e14221
Carlos Eduardo Lima Borges <carloslimaborges@gmail.com>
2016-03-14 03:41:27 -0300

    Step 2
    Step 2

--- program.js
+++ program.js
@@ -1,8 +1,40 @@
 var koa = require('koa');
 var app = koa();
 
+//Solução que pensei
 app.use(function *(next) {
-	this.body = 'hello koa';
+	if (this.path === '/') {
+		this.body = 'hello koa';
+	} else if (this.path === '/404') {
+		this.body = 'page not found';
+	} else if (this.path === '/500') {
+		this.body = 'internal server error';
+	}
+});
+
+//Solução recomendada:
+app.use(function* (next) {
+  if (this.path !== '/') {
+    return yield next;
+  }
+
+  this.body = 'hello koa';
+});
+
+app.use(function* (next) {
+  if (this.path !== '/404') {
+    return yield next;
+  }
+
+  this.body = 'page not found';
+});
+
+app.use(function* (next) {
+  if (this.path !== '/500') {
+    return yield next;
+  }
+
+  this.body = 'internal server error';
 });
 
 var port = process.argv[2];
